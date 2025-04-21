7ee9e6657bedeb1db1524f78b1b1b829b4b0d671
Gabriel Koo <16297877+gabrielkoo@users.noreply.github.com>
2025-04-21 16:26:43 +0800

    Update tailscale-acl.js

--- tailscale-acl.js
+++ tailscale-acl.js
@@ -53,11 +53,13 @@
 					{
 						"name":       "apple-intelligence",
 						"connectors": ["tag:ai-connector"],
-            // FIXME: Remove this line before copying into your "Access Controls" tab in tailscale.
-            // This list is based on 
-            // 1. https://support.apple.com/en-us/101555#:~:text=Apple%20Intelligence%2C%20Siri%2C%20and%20Search
-            // 2. My own experience.
-            // It still doesn't fully work well, refer to https://github.com/tailscale/tailscale/issues/13963.
+/*
+FIXME: Remove this line before copying into your "Access Controls" tab in tailscale.
+This list is based on 
+1. https://support.apple.com/en-us/101555#:~:text=Apple%20Intelligence%2C%20Siri%2C%20and%20Search
+2. My own experience.
+It still doesn't fully work well, refer to https://github.com/tailscale/tailscale/issues/13963.
+*/
 						"domains": [
 							"*.itunes.apple.com",
 							"*.icloud.apple.com",
@@ -77,6 +79,19 @@
 							"cp4.cloudflare.com",
 							"guzzoni.apple.com",
 							"*.smoot.apple.com",
+						],
+					},
+					{
+						"name":       "bedrock",
+						"connectors": ["tag:ai-connector"],
+/*
+If you would like to use _SOME_ Amazon Bedrock models that are country allowlisted.
+*/
+						"domains": [
+							"bedrock.us-east-1.amazonaws.com",
+							"bedrock-runtime.us-east-1.amazonaws.com",
+							"bedrock.us-west-2.amazonaws.com",
+							"bedrock-runtime.us-west-2.amazonaws.com",
 						],
 					},
 				],
